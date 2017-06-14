ae81e2c6a359f8b0b1b2d397857aeec7a0ad7567
NamjinCho <skawls5028@gmail.com>
2017-06-14 14:58:03 +0900

    loggerUpdate

--- main.js
+++ main.js
@@ -4,15 +4,16 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var path = require("path");
 var db;
-
-
-//app.set('views', __dirname+'/');
-app.set('view engine','ejs');
-app.engine('html',require('ejs').renderFile);
-
+var logUpdater = function(req,res,next)
+{
+  console.log('enterenced');
+  next();
+}
 var options = {
   index: "index.html"
 };
+
+app.use(logUpdater);
 
 app.use(express.static(path.join(__dirname, "public"), options));
 app.use(express.static(path.join(__dirname, "js")));
@@ -21,8 +22,6 @@ app.use(express.static(path.join(__dirname, "css")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
-
-
 
 
 
